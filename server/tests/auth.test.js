fdf0d45d244cc343965ae07a09335fafc76d92e4
ivan3455 <trunovivan641@gmail.com>
2024-04-23 00:03:35 +0300

    jenkins added

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -1,22 +1,80 @@
-import { expect } from 'chai';
-import request from 'supertest';
-import server from '../index.js'; // Імпортуємо сервер
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import AWS from "aws-sdk";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-describe('Authentication API Tests', () => {
-  let token; // Змінна для зберігання токену, який буде отриманий після входу
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config(); // завантаження конфігурації з файлу .env
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-  // Тест для перевірки правильності входу користувача
-  it('Should login user and return token', async () => {
-    const res = await request(server)
-      .post('/auth/login')
-      .send({ email: 'ivan@gmail.com', password: 'awdijl912' }); // Перевірте, чи введено правильні дані
-    expect(res.statusCode).to.equal(200); // Очікуємо статус коду 200 (OK)
-    expect(res.body).to.have.property('token'); // Очікуємо, що відповідь містить токен
-    token = res.body.token; // Зберігаємо токен для подальших тестів
-  });
-
-  // Після всіх тестів закриваємо сервер
-  after(async () => {
-    await server.close(); // Закриваємо сервер
-  });
+// S3 config
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
+
+// S3-multer config
+const upload = (bucket_name) => multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: bucket_name,
+    key: function(req, file, cb) {
+      const newFileName = Date.now() + "-" + file.originalname;
+      cb(null, newFileName);
+    }
+  })
+});
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload(process.env.AWS_BUCKET_NAME).single("picture"), register);
+app.post("/posts", upload(process.env.AWS_BUCKET_NAME).single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+let server;
+
+mongoose
+  .connect(process.env.MONGO_URL, { // з'єднання з базою даних MongoDB
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    server = app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    return server; // повертаємо об'єкт сервера для експорту
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
+export default server;
